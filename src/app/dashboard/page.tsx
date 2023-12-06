8a76fe9d0d58fd5ef204fd36d2f592bf58b57993
Nabendu Paul <npaul2173@gmail.com>
2023-12-06 20:43:14 +0530

    updates: Separator and Tabs added as components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,9 @@
 import { Sidebar } from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
 
 export default function Page() {
   return (
@@ -11,6 +16,80 @@ export default function Page() {
         <div className="bg-background">
           <div className="grid lg:grid-cols-5">
             <Sidebar className="hidden lg:block" />
+            <div className="col-span-3 lg:col-span-4 lg:border-l">
+              <div className="h-full px-4 py-6 lg:px-8">
+                <Tabs defaultValue="music" className="h-full space-y-6">
+                  <div className="space-between flex items-center">
+                    <TabsList>
+                      <TabsTrigger value="music" className="relative">
+                        Music
+                      </TabsTrigger>
+                      <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
+                      <TabsTrigger value="live">Live</TabsTrigger>
+                    </TabsList>
+                    <div className="ml-auto mr-4">
+                      <Button>
+                        <PlusCircledIcon className="mr-2 h-4 w-4" />
+                        Add music
+                      </Button>
+                    </div>
+                  </div>
+                  <TabsContent
+                    value="music"
+                    className="border-none p-0 outline-none"
+                  >
+                    <div className="flex items-center justify-between">
+                      <div className="space-y-1">
+                        <h2 className="text-2xl font-semibold tracking-tight">
+                          Listen Now
+                        </h2>
+                        <p className="text-sm text-muted-foreground">
+                          Top picks for you. Updated daily.
+                        </p>
+                      </div>
+                    </div>
+                    <Separator className="my-4" />
+                    <div className="relative">
+                      <ScrollArea>
+                        <div className="flex space-x-4 pb-4"></div>
+                        <ScrollBar orientation="horizontal" />
+                      </ScrollArea>
+                    </div>
+                    <div className="mt-6 space-y-1">
+                      <h2 className="text-2xl font-semibold tracking-tight">
+                        Made for You
+                      </h2>
+                      <p className="text-sm text-muted-foreground">
+                        Your personal playlists. Updated daily.
+                      </p>
+                    </div>
+                    <Separator className="my-4" />
+                    <div className="relative">
+                      <ScrollArea>
+                        <div className="flex space-x-4 pb-4"></div>
+                        <ScrollBar orientation="horizontal" />
+                      </ScrollArea>
+                    </div>
+                  </TabsContent>
+                  <TabsContent
+                    value="podcasts"
+                    className="h-full flex-col border-none p-0 data-[state=active]:flex"
+                  >
+                    <div className="flex items-center justify-between">
+                      <div className="space-y-1">
+                        <h2 className="text-2xl font-semibold tracking-tight">
+                          New Episodes
+                        </h2>
+                        <p className="text-sm text-muted-foreground">
+                          Your favorite podcasts. Updated daily.
+                        </p>
+                      </div>
+                    </div>
+                    <Separator className="my-4" />
+                  </TabsContent>
+                </Tabs>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -192,7 +192,6 @@ export function Sidebar({ className }: SidebarProps) {
                   <path d="M16 6H3" />
                   <path d="M12 18H3" />
                 </svg>
-                {/* {playlist} */}
               </Button>
             </div>
           </ScrollArea>
