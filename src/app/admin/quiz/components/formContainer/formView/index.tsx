b220588f780c8cbe6b3b7b2fcb2a62a2b6c5eea8
Nabendu Paul <npaul2173@gmail.com>
2024-01-01 00:53:22 +0530

    FormTextFIeld UI updated

--- src/app/admin/quiz/components/formContainer/formView/index.tsx
+++ src/app/admin/quiz/components/formContainer/formView/index.tsx
@@ -1,7 +1,7 @@
 import { FormInputField } from "@/components/formFields/FormInputField";
 import { FormNumberInputField } from "@/components/formFields/FormNumberInputField";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { useFilePicker } from "use-file-picker";
 import { FormDataTypes } from "../schema";
@@ -13,10 +13,12 @@ import {
 } from "use-file-picker/validators";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { TopicSelector } from "./Topic";
+import { Button } from "@/components/ui/button";
+import { PlusIcon, SunIcon } from "@radix-ui/react-icons";
 
 export function FormView() {
   const { control } = useFormContext<FormDataTypes>(); // retrieve all hook methods
-  const [coverImage, setCoverImage] = useState(null);
+  const [coverImage, setCoverImage] = useState<string | null>(null);
 
   const { openFilePicker, filesContent, loading, errors } = useFilePicker({
     readAs: "DataURL",
@@ -24,11 +26,24 @@ export function FormView() {
     multiple: true,
     validators: [
       new FileAmountLimitValidator({ max: 1 }),
-      new FileTypeValidator(["jpg", "png"]),
+      new FileTypeValidator(["jpg", "jpeg", "png"]),
       new FileSizeValidator({ maxFileSize: 50 * 1024 * 1024 /* 50 MB */ }),
     ],
   });
   console.log("filesContent---> ", filesContent);
+
+  // useEffect(() => {
+  //   if (filesContent && filesContent.length > 0) {
+  //     const file = filesContent[0];
+  //     const reader = new FileReader();
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => {
+  //       setCoverImage(String(reader.result)); // Store the base64 data
+  //     };
+  //   }
+  // }, [filesContent]);
+
+  console.log(coverImage);
 
   return (
     <div className="max-w-xl mx-auto  space-y-8">
@@ -41,18 +56,26 @@ export function FormView() {
         />
       </AspectRatio> */}
 
-      <button onClick={() => openFilePicker()}>Select files </button>
+      <Button
+        onClick={() => openFilePicker()}
+        variant="outline"
+        className="justify-start space-x-2"
+        // onClick={() => route.push("/admin/quiz")}
+      >
+        <PlusIcon /> <p className="bold"> Cover photo</p>
+      </Button>
 
-      {/* {coverImage && (
+      {filesContent && !!filesContent.length && (
+        // eslint-disable-next-line @next/next/no-img-element
         <Image
-          src={URL.createObjectURL(coverImage)}
+          src={filesContent[0].content}
           alt="Cover Image"
           width={480}
           height={270} // Maintain 16:9 aspect ratio
           className="ml-4 object-cover rounded-lg"
-          layout="fill"
+          // layout="fill"
         />
-      )} */}
+      )}
       <TopicSelector />
       <FormInputField
         name="title"
