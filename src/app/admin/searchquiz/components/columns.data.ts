31c7ea28b1998afe704eb1ef0504af8a17e42e03
Abhishek <abhishek.yps@gmail.com>
2024-01-08 01:37:22 +0530

    refactor: Data file created for columns and unwanted code removed

---
+++ src/app/admin/searchquiz/components/columns.data.ts
@@ -0,0 +1,111 @@
+import { QuizType } from "./searchresult";
+
+export const data: QuizType[] = [
+  {
+    _id: "6571fa798a84135cea498c78",
+    title: "change",
+    topic: "CSS",
+    tags: ["Web Development", "Styling", "Frontend"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description: "Test your knowledge of basic CSS concepts with this quiz.",
+  },
+  {
+    _id: "6571fb9348e8ecb8d1e8a90a",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "6571fba77937f128d98cba8e",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657204c2859ed8d44f05fdba",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "65720501096a3c2ae50c6885",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "6572052eb45ee36e6c79817a",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657206b888d86a55d22f4e19",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657207320210ba3bf42c0e72",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657209459be14cc4e0917920",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657209e377a044426226ea82",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "65720f9f70c828b24100ace0",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "65720fdc70c828b24100ace7",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+];
