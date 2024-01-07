b4a9784e8e2c3e4241a47075c9baf083846fb275
Abhishek <abhishek.yps@gmail.com>
2024-01-07 23:26:41 +0530

    feature: Quiz data added in table

--- src/app/admin/searchquiz/components/searchresult.tsx
+++ src/app/admin/searchquiz/components/searchresult.tsx
@@ -20,7 +20,6 @@ import {
 } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
-// import { Checkbox } from "@/components/ui/checkbox"
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -40,138 +39,235 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { useSearchParams } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 
-const data: Payment[] = [
+const data: QuizType[] = [
   {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    email: "ken99@yahoo.com",
+    _id: "6571fa798a84135cea498c78",
+    title: "change",
+    topic: "CSS",
+    tags: ["Web Development", "Styling", "Frontend"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description: "Test your knowledge of basic CSS concepts with this quiz.",
   },
   {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    email: "Abe45@gmail.com",
+    _id: "6571fb9348e8ecb8d1e8a90a",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
   },
   {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    email: "Monserrat44@gmail.com",
+    _id: "6571fba77937f128d98cba8e",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
   },
   {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    email: "Silas22@gmail.com",
+    _id: "657204c2859ed8d44f05fdba",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
   },
   {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    email: "carmella@hotmail.com",
+    _id: "65720501096a3c2ae50c6885",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "6572052eb45ee36e6c79817a",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657206b888d86a55d22f4e19",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657207320210ba3bf42c0e72",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657209459be14cc4e0917920",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "657209e377a044426226ea82",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "65720f9f70c828b24100ace0",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
+  },
+  {
+    _id: "65720fdc70c828b24100ace7",
+    title: "Python Fundamentals Quiz 2",
+    topic: "Python",
+    tags: ["Python", "Programming", "Fundamentals"],
+    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    description:
+      "Test your knowledge of Python programming fundamentals with this quiz.",
   },
 ];
 
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
+export type QuizType = {
+  _id: string;
+  coverImage: string;
+  tags: string[];
+  title: string;
+  topic: string;
+  description: string;
 };
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<QuizType>[] = [
   {
-    id: "select",
-    header: ({ table }) => (
-      <></>
-      //   <Checkbox
-      //     checked={
-      //       table.getIsAllPageRowsSelected() ||
-      //       (table.getIsSomePageRowsSelected() && "indeterminate")
-      //     }
-      //     onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-      //     aria-label="Select all"
-      //   />
-    ),
+    accessorKey: "coverImage",
+    header: "Cover Image",
     cell: ({ row }) => (
-      <></>
-      //   <Checkbox
-      //     checked={row.getIsSelected()}
-      //     onCheckedChange={(value) => row.toggleSelected(!!value)}
-      //     aria-label="Select row"
-      //   />
+      <Avatar>
+        <AvatarImage src={row.getValue("coverImage")} />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     ),
-    enableSorting: false,
-    enableHiding: false,
   },
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "title",
+    header: "Title",
     cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("status")}</div>
+      <div className="capitalize">{row.getValue("title")}</div>
     ),
   },
   {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+    accessorKey: "topic",
+    header: "Topic",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("topic")}</div>
+    ),
   },
   {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
+    accessorKey: "tags",
+    header: "Tags",
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
-
+      let tags: string[] = row.getValue("tags");
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex gap-1 flex-wrap">
+          {tags.map((item, index) => (
+            <Badge key={index} variant="secondary">
+              {item}
+            </Badge>
+          ))}
+        </div>
       );
     },
   },
+  {
+    accessorKey: "description",
+    header: "Description",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("description")}</div>
+    ),
+  },
+  // {
+  //   accessorKey: "email",
+  //   header: ({ column }) => {
+  //     return (
+  //       <Button
+  //         variant="ghost"
+  //         onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+  //       >
+  //         Email
+  //         <CaretSortIcon className="ml-2 h-4 w-4" />
+  //       </Button>
+  //     );
+  //   },
+  //   cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+  // },
+  // {
+  //   accessorKey: "amount",
+  //   header: () => <div className="text-right">Amount</div>,
+  //   cell: ({ row }) => {
+  //     const amount = parseFloat(row.getValue("amount"));
+
+  //     // Format the amount as a dollar amount
+  //     const formatted = new Intl.NumberFormat("en-US", {
+  //       style: "currency",
+  //       currency: "USD",
+  //     }).format(amount);
+
+  //     return <div className="text-right font-medium">{formatted}</div>;
+  //   },
+  // },
+  // {
+  //   id: "actions",
+  //   enableHiding: false,
+  //   cell: ({ row }) => {
+  //     const payment = row.original;
+
+  //     return (
+  //       <DropdownMenu>
+  //         <DropdownMenuTrigger asChild>
+  //           <Button variant="ghost" className="h-8 w-8 p-0">
+  //             <span className="sr-only">Open menu</span>
+  //             <DotsHorizontalIcon className="h-4 w-4" />
+  //           </Button>
+  //         </DropdownMenuTrigger>
+  //         <DropdownMenuContent align="end">
+  //           <DropdownMenuLabel>Actions</DropdownMenuLabel>
+  //           <DropdownMenuItem
+  //             onClick={() => navigator.clipboard.writeText(payment.id)}
+  //           >
+  //             Copy payment ID
+  //           </DropdownMenuItem>
+  //           <DropdownMenuSeparator />
+  //           <DropdownMenuItem>View customer</DropdownMenuItem>
+  //           <DropdownMenuItem>View payment details</DropdownMenuItem>
+  //         </DropdownMenuContent>
+  //       </DropdownMenu>
+  //     );
+  //   },
+  // },
 ];
 
 export default function SearchResult() {
@@ -188,6 +284,7 @@ export default function SearchResult() {
   React.useEffect(() => {
     if (searchParams?.get("search")?.length) {
       setSearchValue(searchParams?.get("search"));
+      table.getColumn("title")?.setFilterValue(searchParams?.get("search"));
     }
   }, []);
 
@@ -214,12 +311,12 @@ export default function SearchResult() {
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
+          placeholder="Filter quiz..."
           value={searchValue ?? ""}
           onChange={(event) => {
             {
               setSearchValue(event.target.value);
-              table.getColumn("email")?.setFilterValue(event.target.value);
+              table.getColumn("title")?.setFilterValue(event.target.value);
             }
           }}
           className="max-w-sm"
