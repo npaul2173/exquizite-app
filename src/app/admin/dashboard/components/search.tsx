aa413fead987e3491b742315af702ac46225472d
Abhishek <abhishek.yps@gmail.com>
2024-01-07 17:48:05 +0530

    feature: Search component added and demo table added

--- src/app/admin/dashboard/components/search.tsx
+++ src/app/admin/dashboard/components/search.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
   return (
     <div>
-      <div>
+      <div className="flex justify-between ">
         {/* <h2 className="text-2xl font-bold leading-7 sm:truncate sm:text-3xl sm:tracking-tight">
           What will you quiz today ?
         </h2> */}
@@ -9,6 +17,19 @@ export const Search = () => {
         <h1 className="text-2xl font-bold tracking-tight">
           What will you quiz today ?
         </h1>
+        <div className="flex w-full max-w-sm space-x-2 pr-10">
+          <Input
+            placeholder="Search quiz"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+          <Button
+            type="submit"
+            onClick={() => router.push(`/admin/searchquiz?search=${search}`)}
+          >
+            Search
+          </Button>
+        </div>
       </div>
       {/* <div>
         <div className="flex justify-center w-full px-4 py-8 ">
