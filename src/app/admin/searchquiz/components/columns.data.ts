cef72ba448f259c3643ec57003a345cef583d2d6
Abhishek <abhishek.yps@gmail.com>
2024-01-09 23:43:01 +0530

    refactor: interface and data refactor

--- src/app/admin/searchquiz/components/columns.data.ts
+++ src/app/admin/searchquiz/components/columns.data.ts
@@ -1,111 +1,257 @@
-import { QuizType } from "./searchresult";
-
 export const data: QuizType[] = [
   {
-    _id: "6571fa798a84135cea498c78",
+    _id: {
+      $oid: "6571fa798a84135cea498c78",
+    },
     title: "change",
     topic: "CSS",
     tags: ["Web Development", "Styling", "Frontend"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/css-quiz-cover.jpg",
     description: "Test your knowledge of basic CSS concepts with this quiz.",
+    duration: 20,
+    isPublished: false,
+    __v: 0,
+    createdAt: {
+      $date: "2023-12-07T17:01:45.932Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T18:38:05.645Z",
+    },
   },
   {
-    _id: "6571fb9348e8ecb8d1e8a90a",
+    _id: {
+      $oid: "6571fb9348e8ecb8d1e8a90a",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage:
+      "https://images.unsplash.com/photo-1528465424850-54d22f092f9d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: 100,
+    isPublished: false,
+    createdAt: {
+      $date: "2023-12-07T17:06:27.394Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T17:06:27.394Z",
+    },
+    __v: 0,
   },
   {
-    _id: "6571fba77937f128d98cba8e",
+    _id: {
+      $oid: "6571fba77937f128d98cba8e",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: 100,
+    isPublished: false,
+    createdAt: {
+      $date: "2023-12-07T17:06:47.162Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T17:06:47.162Z",
+    },
+    __v: 0,
   },
   {
-    _id: "657204c2859ed8d44f05fdba",
+    _id: {
+      $oid: "657204c2859ed8d44f05fdba",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: 100,
+    isPublished: false,
+    createdAt: {
+      $date: "2023-12-07T17:45:38.124Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T17:45:38.124Z",
+    },
+    __v: 0,
   },
   {
-    _id: "65720501096a3c2ae50c6885",
+    _id: {
+      $oid: "65720501096a3c2ae50c6885",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: 100,
+    isPublished: false,
+    createdAt: {
+      $date: "2023-12-07T17:46:41.584Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T17:46:41.584Z",
+    },
+    __v: 0,
   },
   {
-    _id: "6572052eb45ee36e6c79817a",
+    _id: {
+      $oid: "6572052eb45ee36e6c79817a",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: 100,
+    isPublished: false,
+    createdAt: {
+      $date: "2023-12-07T17:47:26.333Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T17:47:26.333Z",
+    },
+    __v: 0,
   },
   {
-    _id: "657206b888d86a55d22f4e19",
+    _id: {
+      $oid: "657206b888d86a55d22f4e19",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: 100,
+    isPublished: false,
+    createdAt: {
+      $date: "2023-12-07T17:54:00.429Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T17:54:00.429Z",
+    },
+    __v: 0,
   },
   {
-    _id: "657207320210ba3bf42c0e72",
+    _id: {
+      $oid: "657207320210ba3bf42c0e72",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: 100,
+    isPublished: false,
+    createdBy: {
+      $oid: "6571fb7148e8ecb8d1e8a901",
+    },
+    createdAt: {
+      $date: "2023-12-07T17:56:02.635Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T17:56:02.635Z",
+    },
+    __v: 0,
   },
   {
-    _id: "657209459be14cc4e0917920",
+    _id: {
+      $oid: "657209459be14cc4e0917920",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: -1,
+    isPublished: false,
+    createdBy: {
+      $oid: "6571fb7148e8ecb8d1e8a901",
+    },
+    createdAt: {
+      $date: "2023-12-07T18:04:53.928Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T18:04:53.928Z",
+    },
+    __v: 0,
   },
   {
-    _id: "657209e377a044426226ea82",
+    _id: {
+      $oid: "657209e377a044426226ea82",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: -1,
+    isPublished: false,
+    createdBy: {
+      $oid: "6571fb7148e8ecb8d1e8a901",
+    },
+    createdAt: {
+      $date: "2023-12-07T18:07:31.547Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T18:07:31.547Z",
+    },
+    __v: 0,
   },
   {
-    _id: "65720f9f70c828b24100ace0",
+    _id: {
+      $oid: "65720f9f70c828b24100ace0",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: -1,
+    isPublished: false,
+    createdBy: {
+      $oid: "6571fb7148e8ecb8d1e8a901",
+    },
+    createdAt: {
+      $date: "2023-12-07T18:31:59.176Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T18:31:59.176Z",
+    },
+    __v: 0,
   },
   {
-    _id: "65720fdc70c828b24100ace7",
+    _id: {
+      $oid: "65720fdc70c828b24100ace7",
+    },
     title: "Python Fundamentals Quiz 2",
     topic: "Python",
     tags: ["Python", "Programming", "Fundamentals"],
-    coverImage: "https://npauldev.in/ProfilePictureHD.959b411b.webp",
+    coverImage: "https://example.com/python-quiz-cover.jpg",
     description:
       "Test your knowledge of Python programming fundamentals with this quiz.",
+    duration: -1,
+    isPublished: false,
+    createdBy: {
+      $oid: "6571fb7148e8ecb8d1e8a901",
+    },
+    createdAt: {
+      $date: "2023-12-07T18:33:00.709Z",
+    },
+    updatedAt: {
+      $date: "2023-12-07T18:33:00.709Z",
+    },
+    __v: 0,
   },
 ];
---
+++ src/app/admin/searchquiz/components/types.ts
@@ -0,0 +1,22 @@
+interface QuizType {
+  _id: {
+    $oid: string;
+  };
+  title: string;
+  topic: string;
+  tags: string[];
+  coverImage: string;
+  description: string;
+  duration: number;
+  isPublished: boolean;
+  createdBy?: {
+    $oid: string;
+  };
+  createdAt: {
+    $date: string;
+  };
+  updatedAt: {
+    $date: string;
+  };
+  __v: number;
+}
